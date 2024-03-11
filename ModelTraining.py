3c9ae9f642a57773bd6a033d592035ce3c858b62
Haoshen <77655277+sssstforest@users.noreply.github.com>
2024-03-11 06:12:40 +0000

    Added input_target_split function for data preprocessing

--- ModelTraining.py
+++ ModelTraining.py
@@ -1,36 +1,27 @@
 import numpy as np
 import nnfs
 from ModelClasses import *
+from sklearn.model_selection import train_test_split
 
 nnfs.init()
 
-X, y= create_data_mnist('PokemonData')
-# X = np.load('X.npy')
-# y = np.load('y.npy')
-# X_test = np.load('X_test.npy')
-# y_test = np.load('y_test.npy')
+labels = os.listdir('PokemonData')
+# X, y= create_data_mnist('PokemonData')
+X, y = input_target_split('PokemonData', labels)
 
-# Shuffle the training dataset
-keys = np.array(range(X.shape[0]))
-np.random.shuffle(keys)
-X = X[keys]
-y = y[keys]
-
-# Scale and reshape samples
-X = (X.reshape(X.shape[0], -1).astype(np.float32) - 127.5) / 127.5
-# X_test = (X_test.reshape(X_test.shape[0], -1).astype(np.float32) -
-            #  127.5) / 127.5
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.22, random_state=42)
+X_train_reshaped = (X_train.reshape(X_train.shape[0], -1).astype(np.float32) - 127.5) / 127.5
+X_test_reshaped = (X_test.reshape(X_test.shape[0], -1).astype(np.float32) - 127.5) / 127.5
 
 # Instantiate the model
 model = Model()
 
-
 # Add layers
-model.add(Layer_Dense(X.shape[1], 128))
+model.add(Layer_Dense(X_train_reshaped.shape[1], 128))
 model.add(Activation_ReLU())
 model.add(Layer_Dense(128, 128))
 model.add(Activation_ReLU())
-model.add(Layer_Dense(128, 10))
+model.add(Layer_Dense(128, 150))
 model.add(Activation_Softmax())
 
 # Set loss, optimizer and accuracy objects
@@ -44,7 +35,7 @@ model.set(
 model.finalize()
 
 # Train the model
-# model.train(X, y, validation_data=(X_test, y_test),
-#             epochs=10, batch_size=128, print_every=100)
+model.train(X_train_reshaped, y_train, validation_data=(X_test_reshaped, y_test),
+            epochs=10, batch_size=128, print_every=100)
 
-model.save('fashion_mnist1.model')
+model.save('pokemon.model')
